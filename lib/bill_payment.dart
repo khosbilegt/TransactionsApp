39928696f8a7029ca5f4719b52957563d5377e31
khosbilegt <khosookoo@gmail.com>
2023-05-06 21:55:13 +0800

    added upcoming bill payment

--- lib/bill_payment.dart
+++ lib/bill_payment.dart
@@ -15,6 +15,9 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
     color: Color.fromRGBO(106, 157, 152, 1),
   );
 
+  bool isPaying = false;
+  bool isConfirmed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +59,7 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
       children: [
         const SizedBox(height: 150),
         Container(
+          height: MediaQuery.of(context).size.height - 150,
           decoration: BoxDecoration(
             borderRadius: const BorderRadius.all(Radius.circular(25)),
             border: Border.all(
@@ -66,7 +70,7 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
           width: MediaQuery.of(context).size.width,
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-            child: mainContent()
+            child: isConfirmed ? paymentSuccess() : mainContent()
           ),
         )
       ],
@@ -82,9 +86,33 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
   );
   Widget mainContent() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         billInfo(),
         const SizedBox(height: 25),
+        priceInfo(),
+        const SizedBox(height: 25),
+        
+        isPaying 
+        ? Text("") 
+        : const Text(
+          "Select payment method", 
+          textAlign: TextAlign.start,
+          style: TextStyle(fontSize: 16)
+        ),
+        const SizedBox(height: 15),
+        isPaying 
+        ? Text("") 
+        : paymentMethod(),
+        const SizedBox(height: 50),
+        payButton()
+      ],
+    );
+  }
+
+  Widget priceInfo() {
+    return Column(
+      children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -117,12 +145,59 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
             )
           ],
         ),
-        paymentInfo()
       ],
     );
   }
 
   Widget billInfo() {
+    String billFor = "Youtube Premium";
+    if(isPaying) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            child: const Image(
+              height: 30,
+              image: AssetImage("assets/images/youtube.png")
+            )
+          ),
+          const SizedBox(height: 15),
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              text: 'You will pay ',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: billFor,
+                  style: TextStyle(
+                    color: accentColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const TextSpan(
+                  text: " for one month with BCA OneKlik",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]
+      );
+    }
     return Row(
       children:[
         Container(
@@ -142,7 +217,7 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
           children: const [
             Text(
               "Youtube Premium", 
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20)
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18)
             ),
             Text(
               "Feb 28, 2022", 
@@ -154,11 +229,208 @@ class _BillPaymentPageState extends State<BillPaymentPage> {
     );
   }
 
-  Widget paymentInfo() {
+  Widget paymentMethod() {
     return Column(
       children: [
-        Text("payment")
+        paymentButton(0, Icons.payment, "Debit Card"),
+        const SizedBox(height: 25),
+        paymentButton(1, Icons.paypal, "Paypal")
       ],
+    );
+  }
+
+  int activeType = 0;
+  Color activePaymentColor = const Color.fromRGBO(237, 243, 243, 1);
+  Color inactivePaymentColor = const Color.fromRGBO(250, 250, 250, 1);
+  Color accentColor = const Color.fromRGBO(85, 134, 131, 1);
+  Widget paymentButton(int type, IconData icon, String buttonText) {
+    return Container(
+      height: 75,
+      width: MediaQuery.of(context).size.width * 0.9,
+      child: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            activeType = type;
+          });
+        }, 
+        style: ElevatedButton.styleFrom(
+          backgroundColor: type == activeType ? activePaymentColor : inactivePaymentColor,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                color: Colors.white,
+              ),
+              child: Icon(icon, color: type == activeType ? accentColor : Colors.black54),
+            ),
+            const SizedBox(width: 10),
+            Text(buttonText, style: TextStyle(color: type == activeType ? accentColor : Colors.black54)),
+            const SizedBox(width: 10),
+            Icon(type == activeType ?  Icons.radio_button_checked : Icons.radio_button_off, color: accentColor)
+          ],
+        )
+      )
+    );
+  }
+
+  Widget payButton() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: accentColor,
+          elevation: 20,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        onPressed: () {
+          setState(() {
+            if(!isPaying) {
+              isPaying = true;
+            } else {
+              isConfirmed = true;
+            }
+          });
+        }, 
+        child: Text(isPaying ? "Confirm and Pay" : "Pay Now")
+      )
+    );
+  }
+
+  Widget paymentSuccess() {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: Icon(Icons.check_circle, size: 30, color: accentColor)
+        ),
+        const SizedBox(height: 10),
+        Text(
+          "Payment Successful",
+          style: TextStyle(
+            color: accentColor,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          )
+        ),
+        const Text(
+          "Youtube Premium",
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 10),
+        transactionDetails(),
+        priceInfo(),
+        const SizedBox(height: 30),
+        shareButton()
+      ],
+    );
+  }
+
+  bool isHidden = false;
+  Widget transactionDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Transaction Details ", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
+            IconButton(
+              onPressed: (){
+                setState(() {
+                  isHidden = !isHidden;
+                });
+              }, 
+              icon: isHidden ? const Icon(Icons.arrow_drop_down_sharp) : const Icon(Icons.arrow_drop_up_sharp)
+            ),
+          ],
+        ),
+        isHidden ? Text("")
+        : Column(
+          children: [
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Payment Method: ", style: infoStyle),
+                Text("\$1.99", style: priceStyle)
+              ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Status: ", style: infoStyle),
+                Text("\$1.99", style: priceStyle)
+              ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Time: ", style: infoStyle),
+                Text("\$1.99", style: priceStyle)
+              ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Date: ", style: infoStyle),
+                Text("\$1.99", style: priceStyle)
+              ],
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Transaction ID: ", style: infoStyle),
+                Text("\$1.99", style: priceStyle)
+              ],
+            ),
+            const SizedBox(height: 25),
+            CustomPaint(
+              size: const Size(double.infinity, 2),
+              painter: LinePainter(),
+            ),
+            const SizedBox(height: 25),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget shareButton() {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: accentColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+            side: BorderSide(color: accentColor),
+          ),
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        }, 
+        child: const Text("Share Receipt")
+      )
     );
   }
 }
@@ -167,7 +439,7 @@ class LinePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-      ..color = Colors.black54
+      ..color = Colors.black26
       ..strokeWidth = 1;
 
     final start = Offset(0, size.height / 2);
